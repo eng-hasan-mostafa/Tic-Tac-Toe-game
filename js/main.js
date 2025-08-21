0a0bef272951662722a056b1bd341bb568126545
Hasan Mostafa <hasanalimostafa1@gmail.com>
2025-08-21 07:33:28 +0300

    Add gameBoard module

--- js/main.js
+++ js/main.js
@@ -8,7 +8,7 @@ function createUser(name){
 function createPlayer(name, marker){
     
     const {getName} = createUser(name);
-    let  score = 0;
+    let score = 0;
 
     const getMarker = () => marker;
     const getScore = () => score;
@@ -16,3 +16,32 @@ function createPlayer(name, marker){
 
     return {getName, getMarker, getScore, increaseScore}
 }
+
+const gameBoard = (function(){
+
+    const gameBoardGrid = [[null, null, null],
+                           [null, null, null],
+                           [null, null, null]];
+    
+
+
+    const getGameBoard = () => gameBoardGrid;
+
+    const markCell = (xCoordinate, yCoordinate, marker) => {
+        if(xCoordinate >= 3 || yCoordinate >= 3){
+            throw Error('The selected cell is outside the game board range.')
+        }
+        gameBoardGrid[xCoordinate][yCoordinate] = marker;
+    }
+
+    const cleanGameBoard = () => {
+        for(let i = 0; i < 3; i++){
+            for(let j = 0; j < 3; j++){
+                gameBoardGrid[i][j] = null;
+            }
+        }
+    }
+
+
+    return {getGameBoard, markCell, cleanGameBoard};
+})();
